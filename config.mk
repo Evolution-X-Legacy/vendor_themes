6b40d9eae2a58bb215dc17e8bfdac1e9cac0e440
ElDainosor <eldainosor@gmail.com>
2017-12-23 03:55:35 +0000

    [2/2] Add 'GoBold' font family
    Required for lockscreen. Also, added in case of adding more fonts.
    
    based on this commit, credits to beanstown and jhenrique
    https://gitlab.com/PixelExperience/vendor_pixelstyle/commit/faa3d8365bc1fa0d885e3e98f1e609f6e0fb85fb
    
    Change-Id: I6b042e7b2abc27b894cdc6349146c83e0c501b4b

--- config.mk
+++ config.mk
@@ -47,3 +47,12 @@ PRODUCT_PACKAGES += \
     QStileOreoCircleTrim \
     QStileOreoSquircleTrim \
     QStileCircleOutline
+
+# Fonts to copy
+PRODUCT_COPY_FILES += \
+    vendor/themes/fonts/gobold/Gobold.ttf:system/fonts/Gobold.ttf \
+    vendor/themes/fonts/gobold/Gobold-Italic.ttf:system/fonts/Gobold-Italic.ttf \
+    vendor/themes/fonts/gobold/GoboldBold.ttf:system/fonts/GoboldBold.ttf \
+    vendor/themes/fonts/gobold/GoboldBold-Italic.ttf:system/fonts/GoboldBold-Italic.ttf \
+    vendor/themes/fonts/gobold/GoboldThinLight.ttf:system/fonts/GoboldThinLight.ttf \
+    vendor/themes/fonts/gobold/GoboldThinLight-Italic.ttf:system/fonts/GoboldThinLight-Italic.ttf
